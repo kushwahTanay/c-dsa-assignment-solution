62dc3a6a7a8f6a33ce923014b93a0ef93313cbef
kushwahTanay <158543945+kushwahTanay@users.noreply.github.com>
2024-05-03 12:50:38 +0530

    merge sort  assignment-1
    merge sort assignment-1

--- assignment-1/1st.cpp
+++ assignment-1/1st.cpp
@@ -1,12 +1,51 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int  sum(int a, int b){
-    if(a>b) return 0;
-    return a + sum(a+2,b);
+void merge(vector<int>&arr, int low, int mid, int high ){
+    vector<int>temp;
+    int left = low;
+    int right = mid+1;
+    while(left<=mid && right<=high){
+        if(arr[left]>=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+ }
+  while(left<=mid){
+            temp.push_back(arr[left]);
+              left++;
+        }
+        while(right<=high){
+            temp.push_back(arr[right]);
+               right++;
+        }
+        for(int i=low; i<=high; i++){
+            arr[i] = temp[i-low];
+        }
+
 }
-int main(){
-    int a,b;
-    cin>>a>>b;
-    if(a%2==0) a++;
-    cout<<sum(a,b);
+void mergeS(vector<int>&arr,int low, int high){
+     if(low==high) return;
+     int mid = (low+high)/2;
+     mergeS(arr,low,mid);
+     mergeS(arr,mid+1,high);
+     merge(arr,low,mid,high);
+}
+void mergeSort(vector<int>&arr){
+    int n = arr.size();
+    mergeS(arr,0,n-1);
+}
+
+   int main() {
+    vector<int> arr = {1,2,3,4,5};
+    int n = arr.size();
+    mergeSort(arr);
+    for (int i = 0; i < n; i++) {   
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
--- assignment-1/2nd.cpp
+++ assignment-1/2nd.cpp
@@ -1,14 +1,66 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int stairs(int n){
-    if(n==1) return 1;
-    if(n==2) return 2;
-    if(n==3) return 4;
-    if(n==4) return 7;
-    return stairs(n-1)+stairs(n-2)+stairs(n-3)+stairs(n-4);
+void merge(vector<int>&arr, int low, int mid, int high ){
+    vector<int>temp;
+    int left = low;
+    int right = mid+1;
+    while(left<=mid && right<=high){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+ }
+  while(left<=mid){
+            temp.push_back(arr[left]);
+              left++;
+        }
+        while(right<=high){
+            temp.push_back(arr[right]);
+               right++;
+        }
+        for(int i=low; i<=high; i++){
+            arr[i] = temp[i-low];
+        }
+
 }
-int main(){
-    int n;
-    cin>>n;
-    cout<<stairs(n);
+int countInverse(vector<int> &arr, int low, int mid, int high ){
+         int count = 0;
+         int right = mid+1;
+         for(int i=low; i<=mid; i++){
+            while(right <= high && arr[i] > 2*arr[right]) right++;
+            count = count+(right-(mid+1));
+         }
+         return count;
+}
+int mergeS(vector<int>&arr,int low, int high){
+     int count = 0;
+     if(low==high) return count;
+     int mid = (low+high)/2;
+     count += mergeS(arr,low,mid);
+    count += mergeS(arr,mid+1,high);
+     count += countInverse(arr,low,mid,high);
+     merge(arr,low,mid,high);
+     return count;
+}
+int mergeSort(vector<int>&arr){
+    int n = arr.size();
+   
+  return mergeS(arr,0,n-1);
+   
+    
+}
+
+   int main() {
+    vector<int> arr = {2,4,3,5,1};
+    int n = arr.size();
+   int count = mergeSort(arr);
+   cout<<"number of reverse pairs is"<<" "<<count<<" ";
+   return 0;
+    
+    
 }
